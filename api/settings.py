1792360a464dad162d3b308710bcb3442d9cfcb5
Dorian Barboza <dorian.barboza@ueia.pet>
2022-02-11 01:16:48 -0500

    Aplicando las config para postgres y eliminando sqlite

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1%e@i==e%6nva**%1=yh-n#zhrxx283y)tqo+q@p+qbs4gum6k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432
     }
 }
 
